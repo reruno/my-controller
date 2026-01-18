a7ad0522e03b3aa66e50c69d892b1ce1154a9123
michael kan <michael.kan.mail@gmail.com>
2026-01-18 18:40:17 +0100

    update for mycontroller

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 
 	corev1 "k8s.io/api/core/v1"
@@ -15,9 +16,9 @@ import (
 )
 
 const (
-	TargetLabel         = "mycontrollercheck"
-	ExpiryAnnotation    = "mycontrollercheck/expiry"
-	ProcessedAnnotation = "mycontrollercheck/processed" // To prevent infinite loops
+	StartCheckTimeAnnotation = "mycontrollercheck/startchecktime"
+	EndCheckTimeAnnotation   = "mycontrollercheck/endchecktime"
+	CheckNumAnnotaions       = "mycontrollercheck/checknum"
 )
 
 type PodReconciler struct {
@@ -33,65 +34,34 @@ func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 	if err := r.Get(ctx, req.NamespacedName, &pod); err != nil {
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
-
-	// 2. Check if we already finished with this Pod to avoid infinite loop
-	// (Add -> Wait -> Remove -> Add again...)
-	if pod.Annotations[ProcessedAnnotation] == "true" {
+	var ok bool
+	if _, ok = pod.Annotations[EndCheckTimeAnnotation]; ok {
 		return ctrl.Result{}, nil
 	}
-
-	currentTime := time.Now()
-
-	// SCENARIO A: Label is MISSING. We need to add it.
-	if pod.Labels[TargetLabel] != "true" {
-		log.Info("Adding label to pod", "pod", pod.Name)
-
-		if pod.Labels == nil {
-			pod.Labels = map[string]string{}
-		}
-		if pod.Annotations == nil {
-			pod.Annotations = map[string]string{}
-		}
-
-		// Add Label
-		pod.Labels[TargetLabel] = "true"
-		// Set Expiry 15 mins from now
-		expiryTime := currentTime.Add(1 * time.Minute)
-		pod.Annotations[ExpiryAnnotation] = expiryTime.Format(time.RFC3339)
-
-		if err := r.Update(ctx, &pod); err != nil {
+	var checknum int = 0
+	if strNum, ok := pod.Annotations[CheckNumAnnotaions]; ok {
+		num, err := strconv.Atoi(strNum)
+		if err != nil {
 			return ctrl.Result{}, err
 		}
-
-		// IMPORTANT: Tell K8s to trigger this loop again in 15 mins
-		return ctrl.Result{RequeueAfter: 1 * time.Minute}, nil
+		checknum = num
+		checknum++
+		pod.Annotations[CheckNumAnnotaions] = fmt.Sprintf("%d", checknum)
+	} else {
+		pod.Annotations[StartCheckTimeAnnotation] = time.Now().Format(time.RFC3339)
+		checknum = 1
+		pod.Annotations[CheckNumAnnotaions] = fmt.Sprintf("%d", checknum)
 	}
 
-	// SCENARIO B: Label is PRESENT. Check if time is up.
-	expiryStr := pod.Annotations[ExpiryAnnotation]
-	expiryTime, err := time.Parse(time.RFC3339, expiryStr)
-	if err != nil {
-		// If date is invalid, just remove the label to be safe
-		log.Error(err, "Invalid expiry time, cleaning up")
-		expiryTime = currentTime
-	}
-
-	if currentTime.After(expiryTime) {
-		// Time is up! Remove the label.
-		log.Info("Time is up! Removing label", "pod", pod.Name)
-		delete(pod.Labels, TargetLabel)
-		// Mark as processed so we don't add it again immediately
-		pod.Annotations[ProcessedAnnotation] = "true"
-
-		if err := r.Update(ctx, &pod); err != nil {
-			return ctrl.Result{}, err
-		}
+	if checknum >= 100 {
+		pod.Annotations[EndCheckTimeAnnotation] = time.Now().Format(time.RFC3339)
+		r.Update(ctx, &pod)
 		return ctrl.Result{}, nil
+	} else {
+		log.Info("Checks are done", "pod", pod.Name)
+		r.Update(ctx, &pod)
+		return ctrl.Result{RequeueAfter: 1 * time.Second}, nil
 	}
-
-	// Time is NOT up yet (maybe the controller restarted). Requeue for remaining time.
-	remaining := expiryTime.Sub(currentTime)
-	return ctrl.Result{RequeueAfter: remaining}, nil
 }
 
 func (r *PodReconciler) SetupWithManager(mgr ctrl.Manager) error {
